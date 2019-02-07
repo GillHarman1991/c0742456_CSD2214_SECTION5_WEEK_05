aa8566fa71983ba6d935d40f0bd78700c2ac9517
visualstudio <visualstudio@230-PC07>
2019-02-07 11:55:46 -0500

    final commit

--- week5c0742456last/js/main.js
+++ week5c0742456last/js/main.js
@@ -6,5 +6,14 @@
 function arrays() {
     var pens;
     pens = ["red", "green", "blue", "orange", "purple"];
-    console.log(pens);
+ 
+    markers = pens;
+    console.log(markers);
+
+   
+    for (i = 0; i < markers.length; i++) {
+        alert(markers[i]);
+    }
+    alert("length of arrays" + markers.length);
 }
+
